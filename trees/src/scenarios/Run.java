bd8c224445bb193a60932c2ccd78f6395762a90c
FabrizioTavares <63682778+FabrizioTavares@users.noreply.github.com>
2021-05-25 12:56:20 -0300

    First commit

---
+++ trees/src/scenarios/Run.java
@@ -0,0 +1,51 @@
+
+package scenarios;
+
+import avl.*;
+import binary.*;
+import java.util.Scanner;
+
+public class Run {
+    
+   
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        System.out.println("Insert tree type: ");
+        choice = scan.nextInt();
+        
+        switch(choice){
+            case 1:
+                BinaryTree Btree = new BinaryTree();
+                
+                Btree.createRoot(4);
+                Btree.getRoot().addChild();
+                Btree.getRoot().getLeft().setValue(2);
+                Btree.getRoot().addChild();
+                Btree.getRoot().getRight().setValue(6);
+                Btree.getRoot().getLeft().addChild();
+                Btree.getRoot().getLeft().getLeft().setValue(1);
+                Btree.getRoot().getLeft().addChild();
+                Btree.getRoot().getLeft().getRight().setValue(3);
+                Btree.getRoot().getRight().addChild();
+                Btree.getRoot().getRight().getLeft().setValue(5);
+                Btree.getRoot().getRight().addChild();
+                Btree.getRoot().getRight().getRight().setValue(7);
+                
+                Btree.doMenu();
+                
+                break;
+            case 2:
+                AVL avltree = new AVL();
+        
+                avltree.doMenu();
+                break;
+        }
+        
+        
+        
+    }
+    
+}
